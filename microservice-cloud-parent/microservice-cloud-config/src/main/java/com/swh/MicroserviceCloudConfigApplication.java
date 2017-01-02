e5a2d5edc1a1d92140c741e867c72f4b0ea392ce
sanfye <sanfye1018@163.com>
2017-01-02 16:37:36 +0800

    gitHub microservice初始创建

---
+++ microservice-cloud-parent/microservice-cloud-config/src/main/java/com/swh/MicroserviceCloudConfigApplication.java
@@ -0,0 +1,12 @@
+package com.swh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceCloudConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceCloudConfigApplication.class, args);
+	}
+}
